f606085e581a5df6d0f967827922c88157abd218
seven <845418843@qq.com>
2019-03-08 23:15:49 +0800

    this is my first try

---
+++ hardway-code/习题1-第一个程序.py
@@ -0,0 +1,15 @@
+print("hello world!")
+
+print("hello again")
+
+print("I like typing this.")
+
+print("This is fun.")
+
+print('Yay! Printing.')
+
+print("I'd much rather you 'not'.")
+
+print('i "said" do not touch this.')
+
+# 多写一行就多打印一行，使用。。。或八角帽注释，只打印一行。
---
+++ withkids-code/代码清单5-2.py
@@ -0,0 +1,10 @@
+# 5-2
+# 多行注释  如何使用?
+# print "My",
+# print "name",
+# print "is",
+# print "Dave."
+
+# There is no more raw_input() in python3,replaced by input()
+# someName = input("Enter your name: ")
+# print(someName)
